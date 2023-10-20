44cf24fa7c56d1ce303468856b513effc13ffe69
= <=>
2023-10-20 09:41:33 -0600

    footer

---
+++ games.js
@@ -0,0 +1,29 @@
+// Botón de Poker
+document.getElementById("poker-btn").addEventListener("click", function() {
+    window.location.href = "/poker/index.html";
+});
+
+// Botón de Ruleta
+document.getElementById("ruleta-btn").addEventListener("click", function() {
+    window.location.href = "/ruleta/index.html";
+});
+
+// Botón de Blackjack
+document.getElementById("blackjack-btn").addEventListener("click", function() {
+    window.location.href = "/blackjack/index.html";
+});
+
+// Botón de Carta Alta
+document.getElementById("carta-alta-btn").addEventListener("click", function() {
+    window.location.href = "/carta_alta/index.html";
+});
+
+// Botón de Dados
+document.getElementById("dados-btn").addEventListener("click", function() {
+    window.location.href = "/dados/index.html";
+});
+
+// Botón de Dominó
+document.getElementById("domino-btn").addEventListener("click", function() {
+    window.location.href = "/domino/index.html";
+});
